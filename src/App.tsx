da5793a1cb417100292b2059fd6cc905e0d14775
Jonathan Butler <jonathan.butlergetweave.com@Jonathans-MacBook-Pro.local>
2021-03-19 14:11:21 -0700

    adds store, actions, reducers, types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
+const apiKey = process.env.REACT_APP_API_KEY;
 
 const App: FC = () => {
   return (
     <div className="App">
+      <h5>{apiKey}</h5>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/store/types.ts
@@ -0,0 +1,85 @@
+import { Weather, WeatherData } from './types';
+export const GET_WEATHER = 'GET_WEATHER';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ERROR = 'SET_ERROR';
+export const SET_ALERT = 'SET_ALERT';
+
+export interface Weather {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
+}
+
+export interface WeatherData {
+  base: string;
+  clouds: {
+    all: number;
+  };
+  cod: number;
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  dt: number;
+  id: number;
+  main: {
+    feels_like: number;
+    humidity: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    temp: number;
+  };
+  name: string;
+  sys: {
+    country: string;
+    id: number;
+    sunrise: number;
+    sunset: number;
+    type: number;
+  };
+  timezone: number;
+  visibility: number;
+  weather: Weather[];
+  wind: {
+    speed: number;
+    deg: number;
+  };
+}
+
+export interface WeatherError {
+  cod: string;
+  message: string;
+}
+
+export interface WeatherState {
+  data: WeatherData | null;
+  loading: boolean;
+  error: string;
+}
+
+interface GetWeatherAction {
+  type: typeof GET_WEATHER;
+  payload: WeatherData;
+}
+
+interface setLoadingAction {
+  type: typeof SET_LOADING;
+}
+
+interface SetErrorAction {
+  type: typeof SET_ERROR;
+  payload: string;
+}
+
+export type WeatherAction = GetWeatherAction | setLoadingAction | SetErrorAction;
+
+export interface AlertAction {
+  type: typeof SET_ALERT;
+  payload: string;
+}
+
+export interface AlertState {
+  message: string;
+}
